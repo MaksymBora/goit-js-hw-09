f2c0ce22c0a5e5c7c55a6df357a56c13b4540c4b
MaksymBora <maxboraod@gmail.com>
2023-06-11 21:57:06 +0200

    add 1st Task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+
+// Add listener on Start button
+refs.start.addEventListener('click', onClickBgChange);
+let btnStatus = true;
+let intervalId = null;
+
+// The function change background color(randomly) of HTMl's body and disable "Start" button.
+function onClickBgChange() {
+  const changeBgColor = () =>
+    (document.querySelector('body').style.backgroundColor =
+      getRandomHexColor());
+
+  intervalId = setInterval(changeBgColor, 1000);
+  if (btnStatus) {
+    refs.start.setAttribute('disabled', 'disabled');
+    btnStatus = false;
+  }
+  //   document.querySelector('body').style.backgroundColor = getRandomHexColor();
+}
+
+// Add listener on Stop button
+refs.stop.addEventListener('click', onClickStopBgChange);
+
+//The function suspend change color of Html's body and enable "Start" button.
+function onClickStopBgChange() {
+  //
+  clearInterval(intervalId);
+
+  if (!btnStatus) {
+    refs.start.removeAttribute('disabled');
+    btnStatus = true;
+  }
+}

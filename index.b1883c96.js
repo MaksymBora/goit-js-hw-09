de8860fd907ab61babfb0ae0ebda45a577930933
MaksymBora <123206568+MaksymBora@users.noreply.github.com>
2023-06-13 22:30:55 +0000

    Deploying to gh-pages from @ MaksymBora/goit-js-hw-09@d79b1c16caa11b53bc3b3116afaf01977ff0abcd 🚀

--- index.b1883c96.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("mousemove",(e=>{Object.assign(document.documentElement,{style:`\n    --move-x: ${-.005*(e.clientX-window.innerWidth/2)}deg;\n    --move-y: ${-.01*(e.clientY-window.innerWidth/2)}deg;\n    `})}));
-//# sourceMappingURL=index.b1883c96.js.map

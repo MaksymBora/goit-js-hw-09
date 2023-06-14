e7f12b23e99956bf2601c623e4e467dab4b65a68
MaksymBora <123206568+MaksymBora@users.noreply.github.com>
2023-06-14 17:00:21 +0000

    Deploying to gh-pages from @ MaksymBora/goit-js-hw-09@d0754af45d7f40e205c1047329261c879275182e 🚀

---
+++ index.4a95d4e8.js
@@ -0,0 +1,2 @@
+!function(){function e(){var t=document.getElementById("modal");t.style.display="none",t.removeEventListener("click",e)}window.addEventListener("load",(function(){var t;sessionStorage.getItem("modalOpened")||((t=document.getElementById("modal")).style.display="flex",sessionStorage.setItem("modalOpened","true"),t.addEventListener("click",e))}))}();
+//# sourceMappingURL=index.4a95d4e8.js.map
---
+++ index.516c6e08.js
@@ -0,0 +1,2 @@
+function e(){const t=document.getElementById("modal");t.style.display="none",t.removeEventListener("click",e)}window.addEventListener("load",(function(){sessionStorage.getItem("modalOpened")||function(){const t=document.getElementById("modal");t.style.display="flex",sessionStorage.setItem("modalOpened","true"),t.addEventListener("click",e)}()}));
+//# sourceMappingURL=index.516c6e08.js.map
